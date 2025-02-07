b260582a99f90f88a667374ee3cf48f488130cd1
Stasy Bolotnikova <stasybol95@gmail.com>
2025-02-07 10:54:43 +0300

    Models package

---
+++ src/main/java/ru/praktikum/services/qa/scooter/model/IdCourier.java
@@ -0,0 +1,13 @@
+package ru.praktikum.services.qa.scooter.model;
+
+public class IdCourier {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
